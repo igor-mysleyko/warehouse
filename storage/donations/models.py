7de9f425eb3b48a86fe55c26ecd0625fd1f5a88d
igor-mysleyko <85844734+igor-mysleyko@users.noreply.github.com>
2023-03-09 00:39:38 +0300

    create

--- storage/donations/models.py
+++ storage/donations/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Storage(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    price = models.FloatField()
+    size = models.IntegerField(default=55, null=True)
+
+    def __str__(self):
+        return self.name
--- storage/donations/views.py
+++ storage/donations/views.py
@@ -1,19 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Storage
 
 
-goods = [
-    {'name' : 'phone', 'coast' : 2000},
-    {'name' : 'tv', 'coast' : 5000},
-    {'name' : 'PC', 'coast' : 4000},
-    {'name' : 'notebook', 'coast' : 3500},
-    {'name' : 'PS5', 'coast' : 3800},
-]
+# goods = [
+#     {'name' : 'phone', 'coast' : 2000},
+#     {'name' : 'tv', 'coast' : 5000},
+#     {'name' : 'PC', 'coast' : 4000},
+#     {'name' : 'notebook', 'coast' : 3500},
+#     {'name' : 'PS5', 'coast' : 3800},
+# ]
 
 def index(request):
     return render(request,'index.html',)
 
 def all_goods(request):
+    goods = Storage.objects.all()
     return render(request,'all_goods.html',{'goods': goods})
 
 def customer(request):
